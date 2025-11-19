e3a5413a44a8ffe567c48b4d6c05aea38404f489
landau <landau@landau.landau>
2025-11-19 10:27:40 +0400

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """nnnfof"""
 
+
 class Square:
     """erngiueguf"""
     def __init__(self, size=0):
@@ -10,6 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """egefgfgs"""
         return self.__size ** 2
