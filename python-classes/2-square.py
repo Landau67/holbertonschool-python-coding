efe83b9e26118a62240ace09d3e05d39c2850eaf
landau <landau@landau.landau>
2025-11-19 10:26:15 +0400

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""uhbvfbhvfebihfve"""
-
+"""nnnfof"""
 
 class Square:
-    """udfhvyeyerbie"""
+    """erngiueguf"""
     def __init__(self, size=0):
-        """erfeferfre"""
+        """ofevioef"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    def area(self):
+        """egefgfgs"""
+        return self.__size ** 2
